ee0e16b3cc7eeeef452dedb698a8d6a7e22b2c22
ggm <godgunman@gmail.com>
2015-05-12 21:15:51 +0800

    use thread to deal with network

--- SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -113,12 +113,18 @@ public class MainActivity extends ActionBarActivity {
         updateHistory();
         setStoreName();
 
-        Utils.disableStrictMode();
-        String content1 = Utils.fetch("http://maps.googleapis.com/maps/api/geocode/json?address=%E8%87%BA%E5%8C%97%E5%B8%82%E7%BE%85%E6%96%AF%E7%A6%8F%E8%B7%AF%E5%9B%9B%E6%AE%B5%E4%B8%80%E8%99%9F&sensor=false");
-        JSONObject location = Utils.addressToLocation("臺北市羅斯福路四段一號");
+//        Utils.disableStrictMode();
 
-        Log.d("debug", content1);
-        Log.d("debug", location.toString());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String content1 = Utils.fetch("http://maps.googleapis.com/maps/api/geocode/json?address=%E8%87%BA%E5%8C%97%E5%B8%82%E7%BE%85%E6%96%AF%E7%A6%8F%E8%B7%AF%E5%9B%9B%E6%AE%B5%E4%B8%80%E8%99%9F&sensor=false");
+                JSONObject location = Utils.addressToLocation("臺北市羅斯福路四段一號");
+                Log.d("debug", content1);
+                Log.d("debug", location.toString());
+            }
+        }).start();
+
     }
 
     public void setStoreName() {
